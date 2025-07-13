f71963557058198976fd69b883554b3a4fe81918
Sidney <sidney@Sidneys-Mac-mini.local>
2025-07-13 08:23:44 -0700

    ios change

--- metro.config.js
+++ metro.config.js
@@ -2,9 +2,28 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// More aggressive polling configuration
 config.server = {
   ...config.server,
-  port: 8081
+  usePolling: true,
+  pollingInterval: 2000,
+};
+
+// Reduce file watching load
+config.watchFolders = [];
+config.maxWorkers = 1;
+
+// Additional configurations to reduce file watching
+config.resolver = {
+  ...config.resolver,
+  platforms: ['ios', 'android', 'web'],
+};
+
+// Disable file watching entirely
+config.watcher = {
+  ...config.watcher,
+  useWatchman: false,
+  watchman: false,
 };
 
 module.exports = config;
